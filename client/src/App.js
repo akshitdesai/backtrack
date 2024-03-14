04cce69188b9b50a86cac741c6a67f251728782d
Umang070 <patelur77@gmail.com>
2024-03-13 23:46:44 -0400

    login and signup page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,73 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+import { createContext, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Grid, makeStyles } from "@material-ui/core";
+import LoginPage from "./views/Login";
+import SignUpPage from "./views/Signup";
+import Footer from "./views/Footer";
+import MessagePopupBox from "./helper/MessagePopupBox";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={'/stream'} className="App-logo" alt="logo" />
+//       </header>
+//     </div>
+//   );
+// }
+const useStyles = makeStyles((theme) => ({
+  body: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "98vh",
+    paddingTop: "64px",
+    boxSizing: "border-box",
+    width: "100%",
+  },
+}));
+
+export const PopupContext = createContext();
 function App() {
+  const styles = useStyles();
+  const [popup, setPopup] = useState({
+    open: false,
+    severity: "",
+    message: "",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={'/stream'} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <BrowserRouter>
+      <PopupContext.Provider value={setPopup}>
+        <Grid container direction="column">
+          <Grid item>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+          </Grid>
+          {/* <Grid item>
+            <Footer />
+          </Grid> */}
+        </Grid>
+      </PopupContext.Provider>
+      <MessagePopupBox
+        open={popup.open}
+        setOpen={(status) =>
+          setPopup({
+            ...popup,
+            open: status,
+          })
+        }
+        severity={popup.severity}
+        message={popup.message}
+      />
+    </BrowserRouter>
   );
 }
 
